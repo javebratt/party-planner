d4770ee1f13bc6c8393bce6c45e4268c362f6e5f
Jorge Vergara <javebratt@gmail.com>
2021-08-28 11:00:35 -0500

    Adds functionality to signup process

--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import {
+  Auth,
+  authState,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signOut,
+} from '@angular/fire/auth';
+import { doc, Firestore, setDoc } from '@angular/fire/firestore';
+import { User, UserCredential } from '@firebase/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
+  constructor(
+    private readonly auth: Auth,
+    private readonly firestore: Firestore
+  ) {}
 
-  constructor() { }
+  getUser(): Observable<User> {
+    return authState(this.auth);
+  }
+
+  login(email: string, password: string): Promise<UserCredential> {
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  async signup(email: string, password: string): Promise<UserCredential> {
+    try {
+      const newUserCredential: UserCredential =
+        await createUserWithEmailAndPassword(this.auth, email, password);
+
+      const userProfileDocumentReference = doc(
+        this.firestore,
+        `partyPlanner/userProfile/${newUserCredential.user.uid}`
+      );
+
+      await setDoc(userProfileDocumentReference, { email });
+
+      return newUserCredential;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  resetPassword(email: string): Promise<void> {
+    return sendPasswordResetEmail(this.auth, email);
+  }
+
+  logout(): Promise<void> {
+    return signOut(this.auth);
+  }
 }
