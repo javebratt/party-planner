d191a6eb416eda569f5be49961a52e8263dd847f
Jorge Vergara <javebratt@gmail.com>
2021-08-29 13:18:09 -0500

    Implements DLETE

--- src/app/party/detail-party/detail-party.component.ts
+++ src/app/party/detail-party/detail-party.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { LoadingController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
 import { Party } from '../party.model';
 import { PartyService } from '../party.service';
 
@@ -14,7 +14,9 @@ export class DetailPartyComponent implements OnInit {
   constructor(
     private readonly route: ActivatedRoute,
     private readonly partyService: PartyService,
-    private readonly loadingCtrl: LoadingController
+    private readonly loadingCtrl: LoadingController,
+    private readonly alertCtrl: AlertController,
+    private readonly router: Router
   ) {}
 
   ngOnInit() {
@@ -25,7 +27,9 @@ export class DetailPartyComponent implements OnInit {
   initializeParty(partyId: string): void {
     this.partyService.getPartyDetail(partyId).subscribe((party) => {
       this.currentParty = party;
-      this.currentParty.id = partyId;
+      if (this.currentParty) {
+        this.currentParty.id = partyId;
+      }
     });
   }
 
@@ -45,5 +49,37 @@ export class DetailPartyComponent implements OnInit {
       await loading.dismiss();
       console.log(error);
     }
+  }
+
+  async removeParty() {
+    const loading = await this.loadingCtrl.create();
+    try {
+      await loading.present();
+
+      await this.partyService.deleteParty(this.currentParty.id);
+
+      await loading.dismiss();
+
+      this.router.navigateByUrl('party');
+    } catch (error) {
+      await loading.dismiss();
+      console.log(error);
+    }
+  }
+
+  async removePartyAlert(): Promise<void> {
+    const alert = await this.alertCtrl.create({
+      message: `Are you sure you want to delete ${this.currentParty.name}?`,
+      buttons: [
+        { text: 'Cancel', role: 'cancel' },
+        {
+          text: 'Delete Party',
+          handler: () => this.removeParty(),
+        },
+      ],
+    });
+    await alert.present();
+
+    await alert.onDidDismiss();
   }
 }
