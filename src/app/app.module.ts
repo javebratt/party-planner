d6fce60b27d8a0d06f8b4bf9da108eba586e7a0a
Jorge Vergara <javebratt@gmail.com>
2021-09-04 22:46:51 -0500

    Adds the emulator options

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { provideFirestore, getFirestore, enableIndexedDbPersistence } from '@angular/fire/firestore';
+import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
+import {
+  provideFirestore,
+  getFirestore,
+  enableIndexedDbPersistence,
+  connectFirestoreEmulator,
+} from '@angular/fire/firestore';
+import { getStorage, connectStorageEmulator, provideStorage } from '@angular/fire/storage';
+import { provideFunctions, getFunctions, connectFunctionsEmulator } from '@angular/fire/functions';
 import { firebaseConfig } from './credentials';
 
 @NgModule({
@@ -19,11 +26,26 @@ import { firebaseConfig } from './credentials';
     IonicModule.forRoot(),
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideAuth(() => getAuth()),
+    provideAuth(() => {
+      const auth = getAuth();
+      connectAuthEmulator(auth, 'http://localhost:9099');
+      return auth;
+    }),
     provideFirestore(() => {
       const firestore = getFirestore();
+      connectFirestoreEmulator(firestore, 'localhost', 8080); // This needs to be first.
       enableIndexedDbPersistence(firestore);
       return firestore;
+    }),
+    provideStorage(() => {
+      const storage = getStorage();
+      connectStorageEmulator(storage, 'localhost', 9199);
+      return storage;
+    }),
+    provideFunctions(() => {
+      const functions = getFunctions();
+      connectFunctionsEmulator(functions, 'localhost', 5001);
+      return functions;
     }),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
