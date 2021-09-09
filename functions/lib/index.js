bae68f357853e45986dbc471a0cbf5fb86c503c1
Jorge Vergara <javebratt@gmail.com>
2021-09-08 21:38:38 -0500

    WIP

---
+++ functions/lib/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+//# sourceMappingURL=index.js.map
